0e35d5925164c6944c539868b3ce9a07320e735f
Sujon Mir <sujonmhk786@gmail.com>
2022-05-30 07:18:36 +0600

    4th commit

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 function result() {
     let name = document.getElementById("name").value;
     let score = 0;
+
     function scoreUpdateByID(a) {
         if (document.getElementById(a).checked) { score++ }
         return score
@@ -21,7 +22,29 @@ function result() {
     if (score !== 10) {
         score = '0' + score
     }
+    if (typeof(Storage) !== "undefined") {
+        if (localStorage.clickcount) {
+            localStorage.clickcount = Number(localStorage.clickcount) + 1;
+        } else {
+            localStorage.clickcount = 1;
+        }
+        if (localStorage.name || localStorage.score) {
+            delete localStorage.name;
+            delete localStorage.score;
+        }
+        localStorage.name_ = name
+        if (localStorage.bestScore_ < score) {
+            localStorage.bestScore_ = score
+        } else if (localStorage.bestScore_ == undefined) {
+            localStorage.bestScore_ = score
+        } else {
 
-    document.write('<center >' + '<h1 style="font-size: 80px;margin-top: 100px;text-transform:uppercase">' +
-        name + ', your score is: ' + '<div style="color:#F30000;display:block; font-size: 198px">' + score + '</div>' + '</h1>' + '</center>')
+        }
+        localStorage.score_ = score
+
+    } else {
+        alert('Your device is not support localStorage!')
+    }
+
+    document.getElementById('body').innerHTML = '<span class="allOutput">' + name + ', YOUR SCORE IS: <span class="scoreOut"><br>' + score + '</span><br>' + 'AND YOUR BEST SCORE IS: <span class="bestScoreOut"><br>' + localStorage.bestScore_ + '</span><br><a href="index.html" class="retake">retake</a></span>' + '<div class="ttaken">You took the test <span class="clickC">' + localStorage.clickcount + '</span> times in total<div>'
 }
